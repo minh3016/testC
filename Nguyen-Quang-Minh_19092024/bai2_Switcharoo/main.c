908de2e642e691c18a05ad09445019d308209b31
minh-3016 <74865161+minh3016@users.noreply.github.com>
2024-09-17 23:13:53 +0700

    Update

--- Nguyen-Quang-Minh_19092024/bai2_Switcharoo/main.c
+++ Nguyen-Quang-Minh_19092024/bai2_Switcharoo/main.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 #include <string.h>
 
-// Function to swap first and last characters of a string
-const char* switcharoo(char str[]) {
+char* switcharoo(char *str) {
     int len = strlen(str);
 
-    // Condition 1: If the string length is less than two
+    // If the string length is less than two
     if (len < 2) {
         return "Incompatible.";
     }
 
-    // Condition 2: If the first and last characters are the same
-    if (str[0] == str[len - 1]) {
+    //  If the first and last characters are the same
+    if (*str == *(str + len - 1)) {
         return "Two's a pair.";
     }
 
-    // Swap the first and last characters
-    char temp = str[0];
-    str[0] = str[len - 1];
-    str[len - 1] = temp;
+    // Swap the first and last characters 
+    char temp = *str;
+    *str = *(str + len - 1);
+    *(str + len - 1) = temp;
 
-    // Return the modified string
+   
     return str;
+}
+void toString(char *str)
+{
+    printf("%s -> %s \n", str, switcharoo(str));
 }
 
 int main() {
@@ -31,11 +34,10 @@ int main() {
     char str3[] = "Ada";
     char str4[] = "Z";
 
-    printf("%s -> %s\n", str1, switcharoo(str1));  // Output: oellh
-    printf("%s -> %s\n", str2, switcharoo(str2));  // Output: ba
-    printf("%s -> %s\n", str3, switcharoo(str3));  // Output: Incompatible.
-    printf("%s -> %s\n", str4, switcharoo(str4));  // Output: Two's a pair.
+    toString(str1);
+    toString(str2);
+    toString(str3);
+    toString(str4);
 
     return 0;
 }
-
