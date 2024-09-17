51d02d4c194950afbd69e29ba02dd6ea333dda93
minh-3016 <74865161+minh3016@users.noreply.github.com>
2024-09-17 23:13:05 +0700

    Update

--- Nguyen-Quang-Minh_19092024/bai1_Palindrome_Check/main.c
+++ Nguyen-Quang-Minh_19092024/bai1_Palindrome_Check/main.c
@@ -1,43 +1,35 @@
 #include <stdio.h>
 #include <string.h>
-#include <ctype.h>
 
-// Function to check if a string is a palindrome
-int isPalindrome(char str[]) {
+
+int checkPalindrome(char* str) {
     int len = strlen(str);
     int start = 0, end = len - 1;
 
     while (start < end) {
-        // Convert both characters to lowercase for case-insensitivity
-        char leftChar = tolower(str[start]);
-        char rightChar = tolower(str[end]);
-
-        // Check if the characters are the same
-        if (leftChar != rightChar) {
-            return 0; // Not a palindrome
+       
+        if (str[start] != str[end]) {
+            return -1; 
         }
         start++;
         end--;
     }
-    return 1; // It's a palindrome
+    return 1; 
+}
+
+void isPalindrome(char* str) {
+    if (checkPalindrome(str))
+        printf("%s is a palindrome\n", str);
+    else
+        printf("%s is not a palindrome\n", str);
 }
 
 int main() {
+    
     char str1[] = "cmc";
-    char str2[] = "coding";
-
-    // Test case 1
-    if (isPalindrome(str1))
-        printf("%s is a palindrome\n", str1);
-    else
-        printf("%s is not a palindrome\n", str1);
-
-    // Test case 2
-    if (isPalindrome(str2))
-        printf("%s is a palindrome\n", str2);
-    else
-        printf("%s is not a palindrome\n", str2);
+    char str2[] = "Coding";
+    isPalindrome(str1);
+    isPalindrome(str2);
 
     return 0;
 }
-
